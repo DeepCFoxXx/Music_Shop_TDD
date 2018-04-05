c8f0c4ed34c5d3fd9a71e4f0476fcc0f8859b4a2
DeepCFoxXx <learncode23@gmail.com>
2018-04-05 12:23:51 +0100

    shop name passing

---
+++ src/main/java/Shop.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/test/java/TestShop.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestShop {
+
+
+    Shop shop;
+    ISell item;
+
+    @Before
+    public void before() {
+        shop = new Shop("Rock City");
+        item = new MusicBook("Guns and Roses Appetite for Tabs", 2, 12);
+
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Rock City", shop.getName());
+    }
+
+}
